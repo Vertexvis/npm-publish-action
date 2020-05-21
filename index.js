bceff66a385ff66bd24ccda61d18b453427c3a4c
Jeff Murray <jeff.murray@vertexvis.com>
2020-05-21 13:42:43 -0500

    Check script location

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ async function run() {
   const gitPath = await io.which('git', true);
   const npmPath = await io.which('npm', true);
 
+  await exec.exec('ls -l');
   await exec.exec('./npm-publish-action/configure_env.sh', [npmPath, npmRegistry, npmAuth]);
   await exec.exec('./npm-publish-action/publish.sh', [gitPath, npmPath]);
 }
