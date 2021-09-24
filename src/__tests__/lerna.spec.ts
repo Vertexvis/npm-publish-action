7b2767880c3cd25981f22b984b96b5415969440c
Dan Schultz <dan.schultz@vertexvis.com>
2021-09-23 18:00:08 -0600

    Clean up tests (#28)

--- src/__tests__/lerna.spec.ts
+++ src/__tests__/lerna.spec.ts
@@ -11,6 +11,14 @@ import { run } from "../runner";
 import { getEnvVar } from "../env";
 import { configure, publish, versionExists } from "../npm";
 import { createTagAndRef, tagExists } from "../github";
+import {
+  missingGithubTokenEnvVar,
+  missingNpmAuthTokenInput,
+  missingWorkspaceEnvVar,
+  mockEnvVars,
+  mockInput,
+  mockWhich,
+} from "../testing";
 
 const npmAuth = "npm-auth-token";
 const npmRegistry = "https://npm.example.com";
@@ -23,9 +31,9 @@ const package2Path = path.join(workspacePath, "packages/package2");
 const package3Path = path.join(workspacePath, "packages/package3");
 const package4Path = path.join(workspacePath, "packages/package4");
 
-const missingNpmAuthTokenInput = "Missing NPM AUTH TOKEN input";
-const missingWorkspaceEnvVar = "Missing GITHUB_WORKSPACE env var";
-const missingGithubTokenEnvVar = "Missing GITHUB_TOKEN env var";
+const getInputMock = getInput as jest.Mock;
+const whichMock = which as jest.Mock;
+const getEnvVarMock = getEnvVar as jest.Mock;
 
 describe("lerna yarn workspace", () => {
   beforeEach(() => {
@@ -33,18 +41,18 @@ describe("lerna yarn workspace", () => {
   });
 
   it("fails if npm-auth-token input is not provided", async () => {
-    mockInput(undefined, npmRegistry, "true");
-    mockWhich(npmPath);
-    mockEnvVars(workspacePath, githubToken);
+    mockInput(getInputMock, undefined, npmRegistry, "true");
+    mockWhich(whichMock, npmPath);
+    mockEnvVars(getEnvVarMock, workspacePath, githubToken);
 
     await run();
     expect(setFailed).toHaveBeenCalledWith(new Error(missingNpmAuthTokenInput));
   });
 
   it("fails if GITHUB_WORKSPACE env var is not provided", async () => {
-    mockInput(npmAuth, npmRegistry, "true");
-    mockWhich(npmPath);
-    mockEnvVars(undefined, githubToken);
+    mockInput(getInputMock, npmAuth, npmRegistry, "true");
+    mockWhich(whichMock, npmPath);
+    mockEnvVars(getEnvVarMock, undefined, githubToken);
 
     await run();
 
@@ -52,9 +60,9 @@ describe("lerna yarn workspace", () => {
   });
 
   it("fails if GITHUB_TOKEN env var is not provided", async () => {
-    mockInput(npmAuth, npmRegistry, "true");
-    mockWhich(npmPath);
-    mockEnvVars(workspacePath, undefined);
+    mockInput(getInputMock, npmAuth, npmRegistry, "true");
+    mockWhich(whichMock, npmPath);
+    mockEnvVars(getEnvVarMock, workspacePath, undefined);
 
     await run();
 
@@ -62,9 +70,9 @@ describe("lerna yarn workspace", () => {
   });
 
   it("should publish and tag each public project", async () => {
-    mockInput(npmAuth, npmRegistry, undefined);
-    mockWhich(npmPath);
-    mockEnvVars(workspacePath, githubToken);
+    mockInput(getInputMock, npmAuth, npmRegistry, undefined);
+    mockWhich(whichMock, npmPath);
+    mockEnvVars(getEnvVarMock, workspacePath, githubToken);
 
     (versionExists as jest.Mock).mockResolvedValue(false);
     (tagExists as jest.Mock).mockResolvedValue(false);
@@ -107,9 +115,9 @@ describe("lerna yarn workspace", () => {
   });
 
   it("should not publish or tag packages that have been published or tagged", async () => {
-    mockInput(npmAuth, npmRegistry, undefined);
-    mockWhich(npmPath);
-    mockEnvVars(workspacePath, githubToken);
+    mockInput(getInputMock, npmAuth, npmRegistry, undefined);
+    mockWhich(whichMock, npmPath);
+    mockEnvVars(getEnvVarMock, workspacePath, githubToken);
 
     (versionExists as jest.Mock).mockResolvedValue(true);
     (tagExists as jest.Mock).mockResolvedValue(true);
@@ -121,9 +129,9 @@ describe("lerna yarn workspace", () => {
   });
 
   it("should not publish or tag if dry run enabled", async () => {
-    mockInput(npmAuth, npmRegistry, "true");
-    mockWhich(npmPath);
-    mockEnvVars(workspacePath, githubToken);
+    mockInput(getInputMock, npmAuth, npmRegistry, "true");
+    mockWhich(whichMock, npmPath);
+    mockEnvVars(getEnvVarMock, workspacePath, githubToken);
 
     (versionExists as jest.Mock).mockResolvedValue(false);
     (tagExists as jest.Mock).mockResolvedValue(false);
@@ -134,53 +142,3 @@ describe("lerna yarn workspace", () => {
     expect(createTagAndRef).not.toHaveBeenCalled();
   });
 });
-
-function mockInput(
-  npmAuth: string | undefined,
-  npmRegistry: string | undefined,
-  dryRun: string | undefined
-): void {
-  const mock = getInput as jest.Mock;
-
-  if (npmAuth != null) {
-    mock.mockReturnValueOnce(npmAuth);
-  } else {
-    mock.mockImplementationOnce(() => {
-      throw new Error(missingNpmAuthTokenInput);
-    });
-  }
-
-  mock.mockReturnValueOnce(npmRegistry);
-  mock.mockReturnValueOnce(dryRun);
-}
-
-function mockWhich(npmPath: string | undefined): void {
-  if (npmPath != null) {
-    (which as jest.Mock).mockResolvedValueOnce(npmPath);
-  } else {
-    (which as jest.Mock).mockRejectedValueOnce("Missing npm path");
-  }
-}
-
-function mockEnvVars(
-  workspacePath: string | undefined,
-  githubToken: string | undefined
-): void {
-  const mock = getEnvVar as jest.Mock;
-
-  if (workspacePath != null) {
-    mock.mockReturnValueOnce(workspacePath);
-  } else {
-    mock.mockImplementationOnce(() => {
-      throw new Error(missingWorkspaceEnvVar);
-    });
-  }
-
-  if (githubToken != null) {
-    mock.mockReturnValueOnce(githubToken);
-  } else {
-    mock.mockImplementationOnce(() => {
-      throw new Error(missingGithubTokenEnvVar);
-    });
-  }
-}
--- src/__tests__/single-project.spec.ts
+++ src/__tests__/single-project.spec.ts
@@ -11,20 +11,21 @@ import { run } from "../runner";
 import { getEnvVar } from "../env";
 import { configure, publish, versionExists } from "../npm";
 import { createTagAndRef, tagExists } from "../github";
+import { mockEnvVars, mockInput, mockWhich } from "../testing";
 
 const npmAuth = "npm-auth-token";
 const npmRegistry = "https://npm.example.com";
 const npmPath = "path/to/npm";
 const githubToken = "github-token";
 
+const getInputMock = getInput as jest.Mock;
+const whichMock = which as jest.Mock;
+const getEnvVarMock = getEnvVar as jest.Mock;
+
 const workspacePath = path.resolve(
   __dirname,
   "../../test-projects/single-package"
 );
-
-const missingNpmAuthTokenInput = "Missing NPM AUTH TOKEN input";
-const missingWorkspaceEnvVar = "Missing GITHUB_WORKSPACE env var";
-const missingGithubTokenEnvVar = "Missing GITHUB_TOKEN env var";
 
 describe("single project", () => {
   beforeEach(() => {
@@ -32,9 +33,9 @@ describe("single project", () => {
   });
 
   it("should publish and tag project", async () => {
-    mockInput(npmAuth, npmRegistry, undefined);
-    mockWhich(npmPath);
-    mockEnvVars(workspacePath, githubToken);
+    mockInput(getInputMock, npmAuth, npmRegistry, undefined);
+    mockWhich(whichMock, npmPath);
+    mockEnvVars(getEnvVarMock, workspacePath, githubToken);
 
     (versionExists as jest.Mock).mockResolvedValue(false);
     (tagExists as jest.Mock).mockResolvedValue(false);
@@ -53,9 +54,9 @@ describe("single project", () => {
   });
 
   it("should not publish or tag packages that have been published or tagged", async () => {
-    mockInput(npmAuth, npmRegistry, undefined);
-    mockWhich(npmPath);
-    mockEnvVars(workspacePath, githubToken);
+    mockInput(getInputMock, npmAuth, npmRegistry, undefined);
+    mockWhich(whichMock, npmPath);
+    mockEnvVars(getEnvVarMock, workspacePath, githubToken);
 
     (versionExists as jest.Mock).mockResolvedValue(true);
     (tagExists as jest.Mock).mockResolvedValue(true);
@@ -67,9 +68,9 @@ describe("single project", () => {
   });
 
   it("should not publish or tag if dry run enabled", async () => {
-    mockInput(npmAuth, npmRegistry, "true");
-    mockWhich(npmPath);
-    mockEnvVars(workspacePath, githubToken);
+    mockInput(getInputMock, npmAuth, npmRegistry, "true");
+    mockWhich(whichMock, npmPath);
+    mockEnvVars(getEnvVarMock, workspacePath, githubToken);
 
     (versionExists as jest.Mock).mockResolvedValue(false);
 
@@ -79,53 +80,3 @@ describe("single project", () => {
     expect(createTagAndRef).not.toHaveBeenCalled();
   });
 });
-
-function mockInput(
-  npmAuth: string | undefined,
-  npmRegistry: string | undefined,
-  dryRun: string | undefined
-): void {
-  const mock = getInput as jest.Mock;
-
-  if (npmAuth != null) {
-    mock.mockReturnValueOnce(npmAuth);
-  } else {
-    mock.mockImplementationOnce(() => {
-      throw new Error(missingNpmAuthTokenInput);
-    });
-  }
-
-  mock.mockReturnValueOnce(npmRegistry);
-  mock.mockReturnValueOnce(dryRun);
-}
-
-function mockWhich(npmPath: string | undefined): void {
-  if (npmPath != null) {
-    (which as jest.Mock).mockResolvedValueOnce(npmPath);
-  } else {
-    (which as jest.Mock).mockRejectedValueOnce("Missing npm path");
-  }
-}
-
-function mockEnvVars(
-  workspacePath: string | undefined,
-  githubToken: string | undefined
-): void {
-  const mock = getEnvVar as jest.Mock;
-
-  if (workspacePath != null) {
-    mock.mockReturnValueOnce(workspacePath);
-  } else {
-    mock.mockImplementationOnce(() => {
-      throw new Error(missingWorkspaceEnvVar);
-    });
-  }
-
-  if (githubToken != null) {
-    mock.mockReturnValueOnce(githubToken);
-  } else {
-    mock.mockImplementationOnce(() => {
-      throw new Error(missingGithubTokenEnvVar);
-    });
-  }
-}
